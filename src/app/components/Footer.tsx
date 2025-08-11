3ed276b74b40431709e1193e4e03ea27d56f6fc6
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-08-11 12:59:06 +0300

    feat: add site footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mx-auto max-w-4xl p-4 text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} Demons & Archangels
+    </footer>
+  );
+}
